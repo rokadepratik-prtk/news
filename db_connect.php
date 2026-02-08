840c9acc2e429d50c5dca9ac944a157ba5764384
rokadepratik-prtk <rokadepratik.m@gmail.com>
2026-02-09 00:43:22 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "db";          // service name from docker-compose
 $username   = "newsuser";    // must match MYSQL_USER
 $password   = "newpassword"; // must match MYSQL_PASSWORD
-$database   = "newsdb";      // must match MYSQL_DATABASE
+$database   = "news";      // must match MYSQL_DATABASE
 
 $conn = new mysqli($servername, $username, $password, $database);
 
