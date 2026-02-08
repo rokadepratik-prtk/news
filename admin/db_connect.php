cd85c4341d928360fe38a45c0f38b7ce548d0c86
rokadepratik-prtk <rokadepratik.m@gmail.com>
2026-02-08 22:45:16 +0530

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -4,11 +4,9 @@ $username   = "newsuser";    // must match MYSQL_USER
 $password   = "newpassword"; // must match MYSQL_PASSWORD
 $database   = "newsdb";      // must match MYSQL_DATABASE
 
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
-
-
