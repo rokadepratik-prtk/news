d5539611312aa6c298731b3502a23c1a0e112c60
rokadepratik-prtk <rokadepratik.m@gmail.com>
2026-02-09 00:44:15 +0530

    Change database name from 'newsdb' to 'news'

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -2,7 +2,7 @@
 $servername = "db";          // service name from docker-compose
 $username   = "newsuser";    // must match MYSQL_USER
 $password   = "newpassword"; // must match MYSQL_PASSWORD
-$database   = "newsdb";      // must match MYSQL_DATABASE
+$database   = "news";      // must match MYSQL_DATABASE
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
