16e5068cdce9f14d9817ee68e1d70549a202e527
rokadepratik-prtk <rokadepratik.m@gmail.com>
2026-02-08 22:28:28 +0530

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,12 +1,7 @@
 <?php
-$servername = "db";          // service name from docker-compose.yml
-$username   = "newsuser";    // MYSQL_USER
-$password   = "newpassword"; // MYSQL_PASSWORD
-$dbname     = "newsdb";      // MYSQL_DATABASE
+$conn = new mysqli("db", "newsuser", "newpassword", "newsdb");
 
-$cn = new mysqli($servername, $username, $password, $dbname);
-
-if ($cn->connect_error) {
-    die("Connection failed: " . $cn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
